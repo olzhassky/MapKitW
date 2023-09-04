9064d095073b514f370acfc909795fa54f34e196
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-09-04 19:11:10 +0600

    Initial Commit

---
+++ MapKitWUITests/MapKitWUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MapKitWUITestsLaunchTests.swift
+//  MapKitWUITests
+//
+//  Created by Olzhas Zhakan on 04.09.2023.
+//
+
+import XCTest
+
+final class MapKitWUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
